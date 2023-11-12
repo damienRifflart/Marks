b2726bd1b0c7308b49456952d722d4ff960e2dcd
Dams <101669514+ooDams@users.noreply.github.com>
2023-11-12 18:10:02 +0100

    Add files via upload

--- renderer.js
+++ renderer.js
@@ -5,13 +5,8 @@ const passwordInput = document.querySelector('input[name="password"]');
 const headlessSwitch = document.getElementById('switchHeadless');
 const averageButton = document.getElementById("get-average-btn");
 const averageText = document.getElementById("average-text");
+const bestaAverageText = document.getElementById("bestAverage-text");
 const eyeIcon = document.getElementById('eyeIcon');
-
-// Restore credentials when the renderer process is loaded
-ipcRenderer.on('restore-credentials', (event, { username, password }) => {
-    usernameInput.value = username;
-    passwordInput.value = password;
-});
 
 averageButton.addEventListener("click", async () => {
     const username = usernameInput.value;
@@ -24,18 +19,10 @@ averageButton.addEventListener("click", async () => {
         switchValue = false;
     }
 
-    // Save credentials when the "Get Average" button is clicked
-    ipcRenderer.send('save-credentials', { username, password });
-
-    // Wait for the credentials to be saved before proceeding
-    await new Promise(resolve => setTimeout(resolve, 100));
-
-    // Send credentials to the main process
-    ipcRenderer.send("get-average-called", { username, password, headlessSwitch: switchValue });
-
     // Update the average text
     const result = await ipcRenderer.invoke("get-average");
     averageText.innerText = 'Your average is: ' + result;
+    bestaAverageText.innerText = 'Best average: ' + bestaAvg;
 });
 
 eyeIcon.addEventListener('click', () => {
